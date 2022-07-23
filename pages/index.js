e6bde7d9dcaa27b383d9ba2c440a836c175539c8
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-07-23 23:13:41 +0600

    link component navigation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="">
+      <h1>Home Page</h1>
+      <Link href={"/blog"}>
+        <a>Blog</a>
+      </Link>
+      <br />
+      <Link href={"/product"}>
+        <a>Products</a>
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
